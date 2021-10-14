61248d5a0ef129ddaaeed06e24cf8e0ec02623ae
Ayah Kamal <ayahk1999@gmail.com>
2021-10-14 18:38:40 +0300

    added markdown desc

--- netflixAnalysis.py
+++ netflixAnalysis.py
@@ -9,9 +9,20 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 from plotnine import ggplot
+import matplotlib.pyplot as plt
+import missingno as msno
 
 # Import the dataset
 netflix_df = pd.read_csv('netflix_titles.csv')
 netflix_df.head()
+
+
+#--- Cleaning the data ---#
+# We can see from the first 5 rows that some observations
+# contain NaN values.
+# Looking for missing data
+msno.matrix(netflix_df)
+plt.show()
+
 
 
